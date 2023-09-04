a29cba6871fa8877040bdcd4c0c16453375f9851
JustinElMas <justin7b.jjr2014@gmail.com>
2023-09-04 14:53:24 -0500

    Init project

---
+++ src/main/java/com/plaza/mercado/PlazaMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.plaza.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazaMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plaza/mercado/PlazaMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plaza.mercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazaMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
